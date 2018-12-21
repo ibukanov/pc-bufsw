982a9ee8b05efc993ca16f451d16d83a2f200061
Igor Bukanov <igor@mir2.org>
2018-12-21 09:31:44 +0100

    fix buffer description

--- pc-bufsw.el
+++ pc-bufsw.el
@@ -1,7 +1,7 @@
-;; pc-bufsw.el --- PC style quick buffer switcher
+;;; pc-bufsw.el --- PC style quick buffer switcher
 
 ;; This is free and unencumbered software released into the public domain.
-;;`
+;;
 ;; Anyone is free to copy, modify, publish, use, compile, sell, or
 ;; distribute this software, either in source code form or as a compiled
 ;; binary, for any purpose, commercial or non-commercial, and by any
